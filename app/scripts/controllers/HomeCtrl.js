f0367e6dc84aa1241844a171ad98213e85e209de
Laura Slingerland <laura.sling@gmail.com>
2017-11-17 15:02:49 -0500

    Checkpoint 2: Start/Reset Timer Completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,35 @@
 (function() {
-    function HomeCtrl() {
+    function HomeCtrl($interval, $scope) {
+      $scope.session = 'Start';
+      $scope.totalTime = '1500';
+
+      $scope.sessionTime = function(session) {
+        if (session == "Start") {
+          $scope.startSession();
+        } else if (session == "Reset") {
+          $scope.resetSession();
+        }
+      };
+
+      $scope.startSession = function() {
+        $scope.session = "Reset";
+        stop = $interval(function() {
+          if ($scope.totalTime > 0) {
+            $scope.totalTime = $scope.totalTime - 1;
+          } else {
+          }
+        }, 1000, 1500);
+      };
+
+      var theTimer;
+      $scope.resetSession = function() {
+        $scope.totalTime = 1500;
+        $interval.cancel(stop);
+        $scope.session = 'Start';
+      };
     }
 
     angular
         .module('bloctime')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['$interval', '$scope', HomeCtrl]);
 })();
